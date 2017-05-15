fe26b0aaf92f2b2174aa1f5b621f414a72cb8a41
Pietro Mascolo <pietro_mascolo@optum.com>
2017-05-15 16:03:04 +0100

    ADDS calculator assignment

--- quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -9,12 +9,57 @@ import Prop._
 
 abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
 
-  lazy val genHeap: Gen[H] = ???
+  lazy val genHeap: Gen[H] = for {
+    n <- arbitrary[A]
+    h <- frequency((1, Gen.const(empty)), (9, genHeap))
+  } yield insert(n, h)
+
   implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
 
   property("gen1") = forAll { (h: H) =>
     val m = if (isEmpty(h)) 0 else findMin(h)
     findMin(insert(m, h)) == m
+  }
+
+
+
+  property("Deleting the minimal value in a Heap should result in an empty Heap") = forAll { (n: A) =>
+    isEmpty(deleteMin(insert(n, empty)))
+  }
+
+  property("Recusivly finding and deleting elements in a Heap should return same elements") = forAll { (h: H) =>
+    def isSorted(h: H): Boolean =
+      if (isEmpty(h)) true
+      else {
+        val m = findMin(h)
+        val h2 = deleteMin(h)
+        isEmpty(h2) || (m <= findMin(h2) && isSorted(h2))
+      }
+    isSorted(h)
+  }
+
+  property("Minimum of  melded Heap should be min of the min of both heaps") = forAll { (h1: H, h2: H) =>
+    findMin(meld(h1, h2)) == Math.min(findMin(h1), findMin(h2))
+  }
+
+
+  property("Two heaps should be equal if recursively removing min elements result in same elements until empty") = forAll { (h1: H, h2: H) =>
+    def heapEqual(h1: H, h2: H): Boolean =
+      if (isEmpty(h1) && isEmpty(h2)) true
+      else {
+        val m1 = findMin(h1)
+        val m2 = findMin(h2)
+        m1 == m2 && heapEqual(deleteMin(h1), deleteMin(h2))
+      }
+    heapEqual(meld(h1, h2),
+      meld(deleteMin(h1), insert(findMin(h1), h2)))
+  }
+
+  property("Minimum of 2 heaps should be the minimum after dispacing it from heap 1 to 2 and melding both") = forAll { (h1: H, h2: H) =>
+    val m1 = findMin(h1)
+    val m2 = findMin(h2)
+    val m = Math.min(m1,m2)
+    findMin(meld(deleteMin(h1), insert(m, h2))) == m
   }
 
 }
